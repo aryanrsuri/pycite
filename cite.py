6ba0ab592caeed4ec007ef71f80caaa9f4d6f63f
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2024-01-16 22:39:17 -0800

    added extension type validation and fixed bug
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- cite.py
+++ cite.py
@@ -1,39 +1,46 @@
 import re
-import requests,json
+import requests
+import json
+
+
 class Cite(object):
     """ Generate new Citation
         raw: str -- the raw string input
         rtype: str or None -- the valid raw input type (doi or bibtex)
     """
     raw: str
-    parts: dict[str,str] = {}
-    def __init__(self, raw: str, rtype:str) -> None:
-        rtypes: set[str] = {"doi", "bibtex"}
-        if rtype in rtypes:
+    parts: dict[str, str]
+    rtypes: set[str] = {"ris", "bib", "json", "enw"}
+
+    def __init__(self, raw: str, rtype: str) -> None:
+        self.parts: dict[str, str] = {}
+        if rtype in self.rtypes:
             self.raw = raw
             self.parts["rtype"] = rtype
-            if rtype=="doi":
-                return self._fetch_doi()
-            if rtype=="bibtex":
-                return self._parse_bibtex()
-        raise Exception("Invalid input type")
-    def _fetch_doi(self) -> None:
-        response = requests.get('https://doi.org/api/handles/%s' % self.raw.strip());
-        if response.status_code==200:
-            data: str= json.loads(response.text)['values'][0]
-            self.parts["entry"] = data 
-            return None
-        raise Exception("Invalid DOI")
-    def _parse_bibtex(self) -> None:
-        entrytypes={"article","book", "booklet", "conference","inbook","incollection","inproceedings", "manual","mastersthesis", "misc","phdthesis","proceedings","techreport","unpublished"}
-        fieldtypes= {"address", "annote", "author", "booktitle", "Email", "chapter", "crossref", "doi", "edition", "editor", "howpublished", "institution", "journal", "key", "month", "note", "number", "organization", "pages", "publisher", "school", "series", "title", "type", "volume", "year"}
-        etf  = re.search(r"@(\w+){(\w+),([^;]*)}", self.raw)
+            match rtype:
+                case "bib":
+                    self._parse_from_bibtex()
+                case "ris":
+                    self._parse_from_ris()
+                case "json":
+                    self._parse_from_json()
+                case "enw":
+                    self._parse_from_enw()
+                case _:
+                    raise Exception("Invalid input type")
+
+    def _parse_from_bibtex(self) -> None:
+        entrytypes = {"article", "book", "booklet", "conference", "inbook", "incollection", "inproceedings",
+                      "manual", "mastersthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"}
+        fieldtypes = {"address", "annote", "author", "booktitle", "Email", "chapter", "crossref", "doi", "edition", "editor", "howpublished", "institution",
+                      "journal", "key", "month", "note", "number", "organization", "pages", "publisher", "school", "series", "title", "type", "volume", "year"}
+        etf = re.search(r"@(\w+){(\w+),([^;]*)}", self.raw)
         if etf is not None:
             if etf.group(1) in entrytypes:
                 self.parts["entry"] = etf.group(1)
                 self.parts["tag"] = etf.group(2)
             for field in etf.group(3).splitlines():
-                kv = re.search(r"(\w+)={(.+)},",field)
+                kv = re.search(r"(\w+)={(.+)},", field)
                 if kv is not None:
                     if kv.group(1).strip() in fieldtypes:
                         self.parts[kv.group(1).strip()] = kv.group(2)
@@ -41,5 +48,24 @@ class Cite(object):
             raise Exception("Parsing failed")
         return None
 
+    def _parse_from_ris(self) -> None:
+        return None
+
+    def _parse_from_json(self) -> None:
+        return None
+
+    def _parse_from_enw(self) -> None:
+        return None
+
+    def _fetch_doi(self) -> None:
+        response = requests.get(
+            'https://doi.org/api/handles/%s' % self.raw.strip())
+        if response.status_code == 200:
+            data: str = json.loads(response.text)['values'][0]
+            self.parts["entry"] = data
+            return None
+        raise Exception("Invalid DOI")
+
+
 if __name__ == "__main__":
-    pass 
+    pass
--- load.py
+++ load.py
@@ -2,15 +2,12 @@ import os
 from cite import Cite
 for fn in os.listdir("data"):
     file = open(os.path.join("data", fn))
+    ext: str = fn.rsplit(".")[-1]
     try:
-        c = Cite(file.read(), rtype="bibtex")
-        print("%s and its parsed %s" % (fn,c.parts))
+        c = Cite(file.read(), ext)
+        print(c.parts)
     except Exception:
         raise Exception
 
-d = Cite("10.1073/pnas.2214423119", rtype="doi")
-# will fail
-x = Cite("", rtype="doi")
-
-
-
+# d = Cite("10.1073/pnas.2214423119", rtype="doi")
+# x = Cite("", rtype="doi")
