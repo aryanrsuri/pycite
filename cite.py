cb2f6e2fefe0fdc4d80507b2c632a8ee6db847ff
Aryan Suri <arysuri@proton.me>
2024-01-19 22:05:34 -0800

    0.3

--- cite.py
+++ cite.py
@@ -8,6 +8,10 @@ class Cite(object):
         raw: str -- the raw string input
         rtype: str or None -- the valid raw input type (doi or bibtex)
     """
+    entrytypes = {"article", "book", "booklet", "conference", "inbook", "incollection", "inproceedings",
+                  "manual", "mastersthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"}
+    fieldtypes = {"address", "annote", "author", "booktitle", "Email", "chapter", "crossref", "doi", "edition", "editor", "howpublished", "institution",
+                  "journal", "key", "month", "note", "number", "organization", "pages", "publisher", "school", "series", "title", "type", "volume", "year"}
     raw: str
     parts: dict[str, str]
     rtypes: set[str] = {"ris", "bib", "json", "enw"}
@@ -15,8 +19,8 @@ class Cite(object):
     def __init__(self, raw: str, rtype: str) -> None:
         self.parts: dict[str, str] = {}
         if rtype in self.rtypes:
-            self.raw = raw
-            self.parts["rtype"] = rtype
+            self.raw = raw.strip()
+            self.rtype = rtype
             match rtype:
                 case "bib":
                     self._parse_from_bibtex()
@@ -30,25 +34,61 @@ class Cite(object):
                     raise Exception("Invalid input type")
 
     def _parse_from_bibtex(self) -> None:
-        entrytypes = {"article", "book", "booklet", "conference", "inbook", "incollection", "inproceedings",
-                      "manual", "mastersthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"}
-        fieldtypes = {"address", "annote", "author", "booktitle", "Email", "chapter", "crossref", "doi", "edition", "editor", "howpublished", "institution",
-                      "journal", "key", "month", "note", "number", "organization", "pages", "publisher", "school", "series", "title", "type", "volume", "year"}
         etf = re.search(r"@(\w+){(\w+),([^;]*)}", self.raw)
         if etf is not None:
-            if etf.group(1) in entrytypes:
+            if etf.group(1) in self.entrytypes:
                 self.parts["entry"] = etf.group(1)
                 self.parts["tag"] = etf.group(2)
             for field in etf.group(3).splitlines():
                 kv = re.search(r"(\w+)={(.+)},", field)
                 if kv is not None:
-                    if kv.group(1).strip() in fieldtypes:
+                    if kv.group(1).strip() in self.fieldtypes:
                         self.parts[kv.group(1).strip()] = kv.group(2)
         if self.parts == {}:
             raise Exception("Parsing failed")
         return None
 
     def _parse_from_ris(self) -> None:
+        risentrytypes= {
+            "JOUR": "article",
+            "BOOK": "book",
+            "CONF": "conference",
+            "CHAP": "inbook",
+            "THES": ["mastersthesis", "phdthesis"],  # Multiple BibTeX types map to THES
+            "GEN": "manual",
+            "RPRT": "techreport",
+            "UNPB": "unpublished"
+        }
+
+        risfieldtypes = {
+            "AD": "address",
+            "N1": ["annote", "note"],  # Multiple BibTeX types map to N1
+            "AU": "author",
+            "T2": "booktitle",
+            "CN": "chapter",
+            "DO": "doi",
+            "ET": "edition",
+            "ED": "editor",
+            "A1": ["institution", "organization", "school"],  # Multiple BibTeX types map to A1
+            "JA": "journal",
+            "M1": "month",
+            "IS": "number",
+            "SP": "pages",
+            "PB": "publisher",
+            "T3": "series",
+            "T1": "title",
+            "M3": "type",
+            "VL": "volume",
+            "PY": "year"
+        }
+        rtype = re.search(r"TY\s+-\s+(\w+)", self.raw)
+        if rtype is not None:
+            if rtype.group(1) in risentrytypes:
+                self.parts["entry"] = rtype.group(1)
+        etf = re.findall(r"([^TY]\w{2})\s+-\s+(.*)", self.raw)
+        if etf is not None:
+            for match in etf:
+                self.parts[match[0][1:]] = match[1]
         return None
 
     def _parse_from_json(self) -> None:
